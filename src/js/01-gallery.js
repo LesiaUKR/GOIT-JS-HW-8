7f8c5d9655466ebf57b11bdef3f809c07b784cd4
LesiaUKR <solovyova.lesya@gmail.com>
2023-01-09 13:27:18 +0100

    update gallery 01.css

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -21,7 +21,7 @@ function createGalleryMarkup(galleryItems) {
         .map(({ preview, original, description }) => {
             return `
 <li class="gallery__item">
-    <a class="gallery__item" href="${original}">
+    <a class="gallery__link" href="${original}">
         <img
            class="gallery__image"
            src="${preview}"
